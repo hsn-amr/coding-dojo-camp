1e0456df3530290b3fd886dc4ddf77d54c21a93c
Hassan Al-Shammari <73214711+hsn-amr@users.noreply.github.com>
2021-09-13 10:57:50 +0300

    Add files via upload

---
+++ pre-bootcamp-week/CodeChallenge3-Loop-d-loop/LAST_NAME_LoopDLoop.js
@@ -0,0 +1,23 @@
+/*
+    How do we know we need a loop here? because we noticed there is iteration which is pops out a piece of candy
+    What's the starting point of the loop? when runner reaches first 2 miles
+    When should the loop stop? if runner reaches 6 miles
+    How will it know to stop? if miles >= 6 miles
+    What's the incrementing for each iteration of the loop? by 1 mile
+    What variables do we need? miles, limit and speed
+*/
+
+var limit = 6;
+var miles;
+var speed;
+
+for(miles = 1; miles<=limit; miles++){
+    console.log("pop out a piece of candy");
+}
+
+//Stretch Feature 1----------------------------------
+for(miles = 1; miles<=limit; miles++){
+    if(speed > 5.5 || miles%2 == 0){
+        console.log("pop out a piece of candy");
+    }
+}
