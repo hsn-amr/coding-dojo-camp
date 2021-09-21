4a3beed9b3be8bfa08212283deee052a081925f4
Hassan Al-Shammari <73214711+hsn-amr@users.noreply.github.com>
2021-09-21 10:45:25 +0300

    Add files via upload

---
+++ week1/DaysOfTheWeek.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val days = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday", 4 to "Wednesday", 5 to "Thursday", 6 to "Friday", 7 to "Saturday")
+    for (day in days){
+        println(day.value)
+    }
+}
